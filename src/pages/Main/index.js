4342604e6cf6eb84ffc92d8559055d3bff4c15ef
Ruanderson Vieira <ruanderson.vieira@gmail.com>
2019-09-23 05:47:29 -0300

    :construction: get user by github api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const res = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: res.data.name,
+      login: res.data.login,
+      bio: res.data.bio,
+      avatar: res.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
